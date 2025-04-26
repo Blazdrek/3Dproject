dd732c91f79d14ebd7ae2b9ab58c11cbca33bd0d
Blazdrek <blazdrek91@gmail.com>
2025-04-26 13:36:25 +0200

    test

--- Projection points par points/geometry.c
+++ Projection points par points/geometry.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <math.h>
 
@@ -7,6 +8,10 @@
 struct point_s {double x;double y;double z;};
 typedef struct point_s point;
 typedef point vector;
+
+
+struct display_s {int height;int width;};
+typedef struct display_s display;
 
 struct point_2d_s {double x;double y};
 typedef struct point_2d_s point_2d;
@@ -29,7 +34,7 @@ double scalaire_2d(vector_2d a, vector_2d b){
 }
 
 
-point_2d projection(point p){
+point_2d projection(point p,int width,int height,double FOV){
     double x = p.x;
     double y = p.y;
     double z = p.z;
@@ -39,6 +44,7 @@ point_2d projection(point p){
     double y1 = z/x;
 
     //Deuxieme étape : projection sur l'ecran
+    
     
 
 
